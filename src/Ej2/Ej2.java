9ece2554201f299edae825320489ce1dd9a04815
Alberto <albertorodriguezochandio@gmail.com>
2018-05-22 13:02:13 +0200

    Modficacion metodo dibujarFigura en clase TrianguloRectangulo

--- src/Ej2/Ej2.java
+++ src/Ej2/Ej2.java
@@ -6,33 +6,26 @@ import java.io.*;
 public class Ej2 {
     public static void main(String[] args) {
         int c;
-        String cadena="", texto="";
+        String texto="";
         File f = new File("parrafo.txt");
+        
         try{
             FileReader fr = new FileReader(f);
+            FileWriter fw = new FileWriter("otroParrafo.txt");
             c = fr.read();
             while(c!=-1){
-                System.out.println((char)c);
-                if(!"a".equals((char)c) || !"e".equals((char)c) || !"i".equals((char)c) || !"o".equals((char)c) || !"u".equals((char)c))
-                    cadena = cadena + (char)c;
+              if((char)c!='a'&& (char)c!='e' &&(char)c!='i' && (char)c!='o' && (char)c!='u' && (char)c!='E')
+                fw.write(c); 
                 c = fr.read();
             }
             fr.close();
-            
-            //creamos nuevo fichero
-            FileWriter fw = new FileWriter("nuevoParrafo.txt");
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.newLine();
-            bw.write(cadena);
-            
-            bw.close();
             fw.close();
             
             //leemos el nuevo fihero y lo imprimimos por pantalla
-            fr = new FileReader("nuevoParrafo.txt");
+            fr = new FileReader("otroParrafo.txt");
             BufferedReader br = new BufferedReader(fr);
             texto = br.readLine();
-            System.out.println("-- fichero nuevoParrafo --");
+            System.out.println("\n\n-- fichero otroParrafo --");
             while(texto!=null){
                 System.out.println(texto);
                 texto = br.readLine();
